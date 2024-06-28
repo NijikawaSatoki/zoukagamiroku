681b3bc7c290b935516234db84700f158390ead5
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-06-28 04:09:26 -0400

    Created nizi.js

---
+++ js/nizi.js
@@ -0,0 +1,693 @@
+/* =========================================================================
+ * nizi.js "虹の造化者"
+ * =========================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/nizi.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/nizi.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『システム：虹』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "OurSystem"
+function hideSystem() {
+    document.getElementById("OurSystem").style.display = "none";
+}
+
+// Reveals elements with id "OurSystem"
+function revealSystem() {
+    document.getElementById("OurSystem").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━┓
+ * ┃ 『虹川郷鬼』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "SatokiNijikawa"
+function hideSatoki() {
+    document.getElementById("SatokiNijikawa").style.display = "none";
+}
+
+// Reveals elements with id "SatokiNijikawa"
+function revealSatoki() {
+    document.getElementById("SatokiNijikawa").style.display = "unset";
+}
+
+// Hides elements with id "SatokiNijikawaPride"
+function hideSatokiPride() {
+    document.getElementById("SatokiNijikawaPride").style.display = "none";
+}
+
+// Reveals elements with id "SatokiNijikawaPride"
+function revealSatokiPride() {
+    document.getElementById("SatokiNijikawaPride").style.display = "unset";
+}
+
+// Hides elements with id "SatokiNijikawaArt"
+function hideSatokiArt() {
+    document.getElementById("SatokiNijikawaArt").style.display = "none";
+}
+
+// Reveals elements with id "SatokiNijikawaArt"
+function revealSatokiArt() {
+    document.getElementById("SatokiNijikawaArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━┓
+ * ┃ 『ワーモ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "wamo"
+function hideWamo() {
+    document.getElementById("wamo").style.display = "none";
+}
+
+// Reveals elements with id "wamo"
+function revealWamo() {
+    document.getElementById("wamo").style.display = "unset";
+}
+
+// Hides elements with id "wamoPride"
+function hideWamoPride() {
+    document.getElementById("wamoPride").style.display = "none";
+}
+
+// Reveals elements with id "wamoPride"
+function revealWamoPride() {
+    document.getElementById("wamoPride").style.display = "unset";
+}
+
+// Hides elements with id "wamoArt"
+function hideWamoArt() {
+    document.getElementById("wamoArt").style.display = "none";
+}
+
+// Reveals elements with id "wamoArt"
+function revealWamoArt() {
+    document.getElementById("wamoArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━┓
+ * ┃ 『清』のファンクション ┃
+ * ┗━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "kiyoi"
+function hideKiyoi() {
+    document.getElementById("kiyoi").style.display = "none";
+}
+
+// Reveals elements with id "kiyoi"
+function revealKiyoi() {
+    document.getElementById("kiyoi").style.display = "unset";
+}
+
+// Hides elements with id "kiyoiPride"
+function hideKiyoiPride() {
+    document.getElementById("kiyoiPride").style.display = "none";
+}
+
+// Reveals elements with id "kiyoiPride"
+function revealKiyoiPride() {
+    document.getElementById("kiyoiPride").style.display = "unset";
+}
+
+// Hides elements with id "kiyoiArt"
+function hideKiyoiArt() {
+    document.getElementById("kiyoiArt").style.display = "none";
+}
+
+// Reveals elements with id "kiyoiArt"
+function revealKiyoiArt() {
+    document.getElementById("kiyoiArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『自己ケモノ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "MyFursonas"
+function hideFursona() {
+    document.getElementById("MyFursonas").style.display = "none";
+}
+
+// Reveals elements with id "MyFursonas"
+function revealFursona() {
+    document.getElementById("MyFursonas").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━┓
+ * ┃ 『妖狐』のファンクション ┃
+ * ┗━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "youko"
+function hideYouko() {
+    document.getElementById("youko").style.display = "none";
+}
+
+// Reveals elements with id "youko"
+function revealYouko() {
+    document.getElementById("youko").style.display = "unset";
+}
+
+// Hides elements with id "youkoPride"
+function hideYoukoPride() {
+    document.getElementById("youkoPride").style.display = "none";
+}
+
+// Reveals elements with id "youkoPride"
+function revealYoukoPride() {
+    document.getElementById("youkoPride").style.display = "unset";
+}
+
+// Hides elements with id "youkoArt"
+function hideYoukoArt() {
+    document.getElementById("youkoArt").style.display = "none";
+}
+
+// Reveals elements with id "youkoArt"
+function revealYoukoArt() {
+    document.getElementById("youkoArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『虹のオリキャラ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "OriKyara"
+function hideOriginalCharacter() {
+    document.getElementById("OriKyara").style.display = "none";
+}
+
+// Reveals elements with id "OriKyara"
+function revealOriginalCharacter() {
+    document.getElementById("OriKyara").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━┓
+ * ┃ 『アニムス』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "animus"
+function hideAnimus() {
+    document.getElementById("animus").style.display = "none";
+}
+
+// Reveals elements with id "animus"
+function revealAnimus() {
+    document.getElementById("animus").style.display = "unset";
+}
+
+// Hides elements with id "animusPride"
+function hideAnimusPride() {
+    document.getElementById("animusPride").style.display = "none";
+}
+
+// Reveals elements with id "animusPride"
+function revealAnimusPride() {
+    document.getElementById("animusPride").style.display = "unset";
+}
+
+// Hides elements with id "animusArt"
+function hideAnimusArt() {
+    document.getElementById("animusArt").style.display = "none";
+}
+
+// Reveals elements with id "animusArt"
+function revealAnimusArt() {
+    document.getElementById("animusArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━┓
+ * ┃ 『アイン』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "Ayn"
+function hideAyn() {
+    document.getElementById("Ayn").style.display = "none";
+}
+
+// Reveals elements with id "Ayn"
+function revealAyn() {
+    document.getElementById("Ayn").style.display = "unset";
+}
+
+// Hides elements with id "AynPride"
+function hideAynPride() {
+    document.getElementById("AynPride").style.display = "none";
+}
+
+// Reveals elements with id "AynPride"
+function revealAynPride() {
+    document.getElementById("AynPride").style.display = "unset";
+}
+
+// Hides elements with id "AynArt"
+function hideAynArt() {
+    document.getElementById("AynArt").style.display = "none";
+}
+
+// Reveals elements with id "AynArt"
+function revealAynArt() {
+    document.getElementById("AynArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『ボルルマー』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "bolormaa"
+function hideBolormaa() {
+    document.getElementById("bolormaa").style.display = "none";
+}
+
+// Reveals elements with id "bolormaa"
+function revealBolormaa() {
+    document.getElementById("bolormaa").style.display = "unset";
+}
+
+// Hides elements with id "bolormaaPride"
+function hideBolormaaPride() {
+    document.getElementById("bolormaaPride").style.display = "none";
+}
+
+// Reveals elements with id "bolormaaPride"
+function revealBolormaaPride() {
+    document.getElementById("bolormaaPride").style.display = "unset";
+}
+
+// Hides elements with id "bolormaaArt"
+function hideBolormaaArt() {
+    document.getElementById("bolormaaArt").style.display = "none";
+}
+
+// Reveals elements with id "bolormaaArt"
+function revealBolormaaArt() {
+    document.getElementById("bolormaaArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━┓
+ * ┃ 『錯誤』のファンクション ┃
+ * ┗━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "Cuowu"
+function hideCuowu() {
+    document.getElementById("Cuowu").style.display = "none";
+}
+
+// Reveals elements with id "Cuowu"
+function revealCuowu() {
+    document.getElementById("Cuowu").style.display = "unset";
+}
+
+// Hides elements with id "CuowuPride"
+function hideCuowuPride() {
+    document.getElementById("CuowuPride").style.display = "none";
+}
+
+// Reveals elements with id "CuowuPride"
+function revealCuowuPride() {
+    document.getElementById("CuowuPride").style.display = "unset";
+}
+
+// Hides elements with id "CuowuArt"
+function hideCuowuArt() {
+    document.getElementById("CuowuArt").style.display = "none";
+}
+
+// Reveals elements with id "CuowuArt"
+function revealCuowuArt() {
+    document.getElementById("CuowuArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『ハップ・Ｐ・エモ＝ション』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "HapP_Emoshun"
+function hideHap() {
+    document.getElementById("HapP_Emoshun").style.display = "none";
+}
+
+// Reveals elements with id "HapP_Emoshun"
+function revealHap() {
+    document.getElementById("HapP_Emoshun").style.display = "unset";
+}
+
+// Hides elements with id "HapP_EmoshunPride"
+function hideHapPride() {
+    document.getElementById("HapP_EmoshunPride").style.display = "none";
+}
+
+// Reveals elements with id "HapP_EmoshunPride"
+function revealHapPride() {
+    document.getElementById("HapP_EmoshunPride").style.display = "unset";
+}
+
+// Hides elements with id "HapP_EmoshunArt"
+function hideHapArt() {
+    document.getElementById("HapP_EmoshunArt").style.display = "none";
+}
+
+// Reveals elements with id "HapP_EmoshunArt"
+function revealHapArt() {
+    document.getElementById("HapP_EmoshunArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━┓
+ * ┃ 『イドラ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "idola"
+function hideIdola() {
+    document.getElementById("idola").style.display = "none";
+}
+
+// Reveals elements with id "idola"
+function revealIdola() {
+    document.getElementById("idola").style.display = "unset";
+}
+
+// Hides elements with id "idolaPride"
+function hideIdolaPride() {
+    document.getElementById("idolaPride").style.display = "none";
+}
+
+// Reveals elements with id "idolaPride"
+function revealIdolaPride() {
+    document.getElementById("idolaPride").style.display = "unset";
+}
+
+// Hides elements with id "idolaArt"
+function hideIdolaArt() {
+    document.getElementById("idolaArt").style.display = "none";
+}
+
+// Reveals elements with id "idolaArt"
+function revealIdolaArt() {
+    document.getElementById("idolaArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『郷鬼の式神達』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "MyShikigami"
+function hideShikigami() {
+    document.getElementById("MyShikigami").style.display = "none";
+}
+
+// Reveals elements with id "MyShikigami"
+function revealShikigami() {
+    document.getElementById("MyShikigami").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『エリサベト・ピルツハライシュヴィリ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishvili"
+function hideElisabed() {
+    document.getElementById("ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishvili").style.display = "none";
+}
+
+// Reveals elements with id "ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishvili"
+function revealElisabed() {
+    document.getElementById("ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishvili").style.display = "unset";
+}
+
+// Hides elements with id "ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliPride"
+function hideElisabedPride() {
+    document.getElementById("ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliPride").style.display = "none";
+}
+
+// Reveals elements with id "ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliPride"
+function revealElisabedPride() {
+    document.getElementById("ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliPride").style.display = "unset";
+}
+
+// Hides elements with id "ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliArt"
+function hideElisabedArt() {
+    document.getElementById("ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliArt").style.display = "none";
+}
+
+// Reveals elements with id "ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliArt"
+function revealElisabedArt() {
+    document.getElementById("ElisabedKhidirbegishviliGarsevanishviliPirtskhalaishviliArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『オルハ・ユヒミウナ・リトウィナ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "OlhaYukhymivnaLytvynaKostyuchenko"
+function hideOlha() {
+    document.getElementById("OlhaYukhymivnaLytvynaKostyuchenko").style.display = "none";
+}
+
+// Reveals elements with id "OlhaYukhymivnaLytvynaKostyuchenko"
+function revealOlha() {
+    document.getElementById("OlhaYukhymivnaLytvynaKostyuchenko").style.display = "unset";
+}
+
+// Hides elements with id "OlhaYukhymivnaLytvynaKostyuchenkoPride"
+function hideOlhaPride() {
+    document.getElementById("OlhaYukhymivnaLytvynaKostyuchenkoPride").style.display = "none";
+}
+
+// Reveals elements with id "OlhaYukhymivnaLytvynaKostyuchenkoPride"
+function revealOlhaPride() {
+    document.getElementById("OlhaYukhymivnaLytvynaKostyuchenkoPride").style.display = "unset";
+}
+
+// Hides elements with id "OlhaYukhymivnaLytvynaKostyuchenkoArt"
+function hideOlhaArt() {
+    document.getElementById("OlhaYukhymivnaLytvynaKostyuchenkoArt").style.display = "none";
+}
+
+// Reveals elements with id "OlhaYukhymivnaLytvynaKostyuchenkoArt"
+function revealOlhaArt() {
+    document.getElementById("OlhaYukhymivnaLytvynaKostyuchenkoArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━┓
+ * ┃ 『孟聖姫』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "MaengSongHee"
+function hideSonghee() {
+    document.getElementById("MaengSongHee").style.display = "none";
+}
+
+// Reveals elements with id "MaengSongHee"
+function revealSonghee() {
+    document.getElementById("MaengSongHee").style.display = "unset";
+}
+
+// Hides elements with id "MaengSongHeePride"
+function hideSongheePride() {
+    document.getElementById("MaengSongHeePride").style.display = "none";
+}
+
+// Reveals elements with id "MaengSongHeePride"
+function revealSongheePride() {
+    document.getElementById("MaengSongHeePride").style.display = "unset";
+}
+
+// Hides elements with id "MaengSongHeeArt"
+function hideSongheeArt() {
+    document.getElementById("MaengSongHeeArt").style.display = "none";
+}
+
+// Reveals elements with id "MaengSongHeeArt"
+function revealSongheeArt() {
+    document.getElementById("MaengSongHeeArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『ぬゑさか岸姫』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "KishihimeNuwesaka"
+function hideKishihime() {
+    document.getElementById("KishihimeNuwesaka").style.display = "none";
+}
+
+// Reveals elements with id "KishihimeNuwesaka"
+function revealKishihime() {
+    document.getElementById("KishihimeNuwesaka").style.display = "unset";
+}
+
+// Hides elements with id "KishihimeNuwesakaPride"
+function hideKishihimePride() {
+    document.getElementById("KishihimeNuwesakaPride").style.display = "none";
+}
+
+// Reveals elements with id "KishihimeNuwesakaPride"
+function revealKishihimePride() {
+    document.getElementById("KishihimeNuwesakaPride").style.display = "unset";
+}
+
+// Hides elements with id "KishihimeNuwesakaArt"
+function hideKishihimeArt() {
+    document.getElementById("KishihimeNuwesakaArt").style.display = "none";
+}
+
+// Reveals elements with id "KishihimeNuwesakaArt"
+function revealKishihimeArt() {
+    document.getElementById("KishihimeNuwesakaArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『イリニシュナ・マカロフナ・オブラズツォファ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "IlyinishnaMakarovnaObraztsovaSluchevskaya"
+function hideIlyinishna() {
+    document.getElementById("IlyinishnaMakarovnaObraztsovaSluchevskaya").style.display = "none";
+}
+
+// Reveals elements with id "IlyinishnaMakarovnaObraztsovaSluchevskaya"
+function revealIlyinishna() {
+    document.getElementById("IlyinishnaMakarovnaObraztsovaSluchevskaya").style.display = "unset";
+}
+
+// Hides elements with id "IlyinishnaMakarovnaObraztsovaSluchevskayaPride"
+function hideIlyinishnaPride() {
+    document.getElementById("IlyinishnaMakarovnaObraztsovaSluchevskayaPride").style.display = "none";
+}
+
+// Reveals elements with id "IlyinishnaMakarovnaObraztsovaSluchevskayaPride"
+function revealIlyinishnaPride() {
+    document.getElementById("IlyinishnaMakarovnaObraztsovaSluchevskayaPride").style.display = "unset";
+}
+
+// Hides elements with id "IlyinishnaMakarovnaObraztsovaSluchevskayaArt"
+function hideIlyinishnaArt() {
+    document.getElementById("IlyinishnaMakarovnaObraztsovaSluchevskayaArt").style.display = "none";
+}
+
+// Reveals elements with id "IlyinishnaMakarovnaObraztsovaSluchevskayaArt"
+function revealIlyinishnaArt() {
+    document.getElementById("IlyinishnaMakarovnaObraztsovaSluchevskayaArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『サンダ・カイン・テインギ・テ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "SandaKhaingTheingiHtet"
+function hideSanda() {
+    document.getElementById("SandaKhaingTheingiHtet").style.display = "none";
+}
+
+// Reveals elements with id "SandaKhaingTheingiHtet"
+function revealSanda() {
+    document.getElementById("SandaKhaingTheingiHtet").style.display = "unset";
+}
+
+// Hides elements with id "SandaKhaingTheingiHtetPride"
+function hideSandaPride() {
+    document.getElementById("SandaKhaingTheingiHtetPride").style.display = "none";
+}
+
+// Reveals elements with id "SandaKhaingTheingiHtetPride"
+function revealSandaPride() {
+    document.getElementById("SandaKhaingTheingiHtetPride").style.display = "unset";
+}
+
+// Hides elements with id "SandaKhaingTheingiHtetArt"
+function hideSandaArt() {
+    document.getElementById("SandaKhaingTheingiHtetArt").style.display = "none";
+}
+
+// Reveals elements with id "SandaKhaingTheingiHtetArt"
+function revealSandaArt() {
+    document.getElementById("SandaKhaingTheingiHtetArt").style.display = "unset";
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『スカーレット・ウィリアムズ』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Hides elements with id "ScarlettWilliams"
+function hideScarlett() {
+    document.getElementById("ScarlettWilliams").style.display = "none";
+}
+
+// Reveals elements with id "ScarlettWilliams"
+function revealScarlett() {
+    document.getElementById("ScarlettWilliams").style.display = "unset";
+}
+
+// Hides elements with id "ScarlettWilliamsPride"
+function hideScarlettPride() {
+    document.getElementById("ScarlettWilliamsPride").style.display = "none";
+}
+
+// Reveals elements with id "ScarlettWilliamsPride"
+function revealScarlettPride() {
+    document.getElementById("ScarlettWilliamsPride").style.display = "unset";
+}
+
+// Hides elements with id "ScarlettWilliamsArt"
+function hideScarlettArt() {
+    document.getElementById("ScarlettWilliamsArt").style.display = "none";
+}
+
+// Reveals elements with id "ScarlettWilliamsArt"
+function revealScarlettArt() {
+    document.getElementById("ScarlettWilliamsArt").style.display = "unset";
+}
