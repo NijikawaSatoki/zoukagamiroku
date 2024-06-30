3910e462d8676518860669cfc9ae9a3097daa114
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-06-30 19:24:15 -0400

    Created hutagozima.js

---
+++ js/hutagozima.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * hutagozima.js "二子島連合共和国"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/hutagozima.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/hutagozima.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
