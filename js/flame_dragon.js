cdcfcfa24e1069f00ed4d1932d9cbaa9628138b5
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-06-30 19:23:46 -0400

    Created flame_dragon.js

---
+++ js/flame_dragon.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * flame_dragon.js "炎龍帝國"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/flame_dragon.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/flame_dragon.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
