dcfb14bd899566731ecd249194b75dbb62c58f48
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-01 14:01:56 -0400

    Created battle_item.js

---
+++ js/battle_item.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * battle_item.js "戦闘のアイテム"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/battle_item.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/battle_item.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
