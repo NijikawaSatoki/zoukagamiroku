7751cfd854d08b53ef532d78d5c265e9e3538599
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-01 14:30:51 -0400

    Created monster.js

---
+++ js/monster.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * monster.js "妖獣の書"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/monster.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/monster.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
