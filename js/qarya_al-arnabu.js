5c5dfb351f9b876593d51ec5a1dbce1588684776
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-01 14:43:04 -0400

    Created qarya_al-arnabu.js

---
+++ js/qarya_al-arnabu.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * qarya_al-arnabu.js "ウサギの里"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/qarya_al-arnabu.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/qarya_al-arnabu.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
