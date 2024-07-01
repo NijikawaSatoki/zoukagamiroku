ec2cb0de8ce493689d16a2ed9f75fbf7e78d13aa
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-01 14:50:38 -0400

    Created stried.prar.js

---
+++ js/stried_prar.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * stried_prar.js "ストリエドプラル州"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/stried_prar.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/stried_prar.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
