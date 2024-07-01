7251ba1796596848a014137639a2577c8b07090f
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-01 14:07:37 -0400

    Created claeq-swary.js

---
+++ js/claeq-swary.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * claeq-swary.js "クレーク＝スワリーのカウンシル・エリア"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/claeq-swary.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/claeq-swary.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
