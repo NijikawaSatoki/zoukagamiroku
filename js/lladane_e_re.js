a71a461c2d5288eb0f0d985db51251c9e1c46b1c
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-01 14:23:39 -0400

    Created lladane_e_re.js

---
+++ js/lladane_e_re.js
@@ -0,0 +1,35 @@
+/* ============================================================================
+ * lladane_e_re.js "新・ラダナ市"
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ * [ja]
+ * このファイルはHTML「/html/lladane_e_re.html」の為にデザインしました。
+ * 楽しみを持って。
+ * 
+ * [en]
+ * This script file is for the HTML file located at
+ *     /html/lladane_e_re.html
+ * 
+ * Enjoy!
+ * 
+ */
+
+/* ┏━━━━━━━━━━━┓
+ * ┃ 『』のファンクション ┃
+ * ┗━━━━━━━━━━━┛
+ * 
+ */
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『全ての為に』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+// Function for the "Hide all" / 『全てを隠す』 button
+function hideAll() {}
+
+// Function for the "Reveal all" / 『全てを現す』 button
+function revealAll() {}
