c7c321a685a0a46a76d744f3bb280f6e5d0eb84a
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-06 20:36:46 -0400

    Armed hide and reveal button

--- js/ancient.js
+++ js/ancient.js
@@ -16,11 +16,483 @@
  * 
  */
 
-/* ┏━━━━━━━━━━━┓
- * ┃ 『』のファンクション ┃
- * ┗━━━━━━━━━━━┛
+/* ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『古い都の町人、古代と現代』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━┛
  * 
  */
+
+function hideTownfolk() {
+    document.getElementById("TheFormerCapitalThenAndNow").style.display = "none";
+    return 0;
+}
+
+function revealTownfolk() {
+    document.getElementById("TheFormerCapitalThenAndNow").style.display = "unset";
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『柏嵜法牌香』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideKashizakiNoHakaruNoFudakau() {
+    return 0;
+}
+
+function revealKashizakiNoHakaruNoFudakau() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『遠呂智蛇揺雨』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideOrochiNoHebiNoYurusame() {
+    return 0;
+}
+
+function revealOrochiNoHebiNoYurusame() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『平也醒上丸』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideTairaNoYasameNoKamimaru() {
+    return 0;
+}
+
+function revealTairaNoYasameNoKamimaru() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『夜叉町鐵奈我沙』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideYashamachiNoKuroganeNoNagasa() {
+    return 0;
+}
+
+function revealYashamachiNoKuroganeNoNagasa() {
+    return 0;
+}
+
+// ****************************************************************************
+// `hide0` and `reveal0` functions for hideAll() and revealAll(), respectively
+// ****************************************************************************
+
+function hide0() {
+    hideTownfolk();
+    hideKashizakiNoHakaruNoFudakau();
+    hideOrochiNoHebiNoYurusame();
+    hideTairaNoYasameNoKamimaru();
+    hideYashamachiNoKuroganeNoNagasa();
+    return 0;
+}
+
+function reveal0() {
+    revealTownfolk();
+    revealKashizakiNoHakaruNoFudakau();
+    revealOrochiNoHebiNoYurusame();
+    revealTairaNoYasameNoKamimaru();
+    revealYashamachiNoKuroganeNoNagasa();
+    return 0;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/* ┏━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『神關里の元帝王』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideFormerEmperors() {
+    return 0;
+}
+
+function revealFormerEmperors() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『一条久二乃良宗』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideIchijoNoKuniNoNoramune() {
+    return 0;
+}
+
+function revealIchijoNoKuniNoNoramune() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『二条清八太郎』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideNijoNoKiyoNoYataro() {
+    return 0;
+}
+
+function revealNijoNoKiyoNoYataro() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『三條麹丸雨』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideSanjoNoKoujiNoMarusame() {
+    return 0;
+}
+
+function revealSanjoNoKoujiNoMarusame() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『源蜘彩歌子』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideMinamotoNoKumoNoAyakashi() {
+    return 0;
+}
+
+function revealMinamotoNoKumoNoAyakashi() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『平榊間美智』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideTairaNoSakamaNoYoshitomo() {
+    return 0;
+}
+
+function revealTairaNoSakamaNoYoshitomo() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『藤原裁峰義』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideFujiwaraNoSaiNoMineyoshi() {
+    return 0;
+}
+
+function revealFujiwaraNoSaiNoMineyoshi() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『橘藪根岸仁』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideTachibanaNoYabuneNoKishihito() {
+    return 0;
+}
+
+function revealTachibanaNoYabuneNoKishihito() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『物部鑓殺奈』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideMononobeNoYariNoSetsuna() {
+    return 0;
+}
+
+function revealMononobeNoYariNoSetsuna() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『九条纖本盛』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideKujoNoShinaNoMotomori() {
+    return 0;
+}
+
+function revealKujoNoShinaNoMotomori() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『蘇我牀遣仁』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideSogaNoYukaNoYaruhito() {
+    return 0;
+}
+
+function revealSogaNoYukaNoYaruhito() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『足利畔蒜毬二』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideAshikagaNoKuroneNoMariji() {
+    return 0;
+}
+
+function revealAshikagaNoKuroneNoMariji() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『鷹司闇光之郞』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideTakatsukasaNoYamiNoMitsunoro() {
+    return 0;
+}
+
+function revealTakatsukasaNoYamiNoMitsunoro() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『德川衣世探重』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideTokugawaNoIyoNoSagashishige() {
+    return 0;
+}
+
+function revealTokugawaNoIyoNoSagashishige() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『大炊御門鈴鵜麻』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideOinomikadoNoSuzuNoUma() {
+    return 0;
+}
+
+function revealOinomikadoNoSuzuNoUma() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『西園寺綯清宗』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideSaionjiNoNauNoKiyomune() {
+    return 0;
+}
+
+function revealSaionjiNoNauNoKiyomune() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━┓
+ * ┃ 『醍醐猿概』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideDaigoNoSaruNoOmune() {
+    return 0;
+}
+
+function revealDaigoNoSaruNoOmune() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『今出川裙兔雪丸』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideImadegawaNoGuntoNoSetsumaru() {
+    return 0;
+}
+
+function revealImadegawaNoGuntoNoSetsumaru() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『德大寺誇宗沙』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideTokudaijiNoHokoruNoMunesa() {
+    return 0;
+}
+
+function revealTokudaijiNoHokoruNoMunesa() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━┓
+ * ┃ 『近衛鞠染鮫』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideKonoeNoMariNoSomezame() {
+    return 0;
+}
+
+function revealKonoeNoMariNoSomezame() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『菊亭馬海春盛』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideKikuteiNoUmakaiNoHarumori() {
+    return 0;
+}
+
+function revealKikuteiNoUmakaiNoHarumori() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━━┓
+ * ┃ 『花山院墨染花清』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideKasannoinNoSumizomeNoHananokiyo() {
+    return 0;
+}
+
+function revealKasannoinNoSumizomeNoHananokiyo() {
+    return 0;
+}
+
+/* ┏━━━━━━━━━━━━━━━━━┓
+ * ┃ 『遊坂茜志奴呂』のファンクション ┃
+ * ┗━━━━━━━━━━━━━━━━━┛
+ * 
+ */
+
+function hideYuuzakaNoAkaneNoShinuro() {
+    return 0;
+}
+
+function revealYuuzakaNoAkaneNoShinuro() {
+    return 0;
+}
+
+// ****************************************************************************
+// `hide1` and `reveal1` functions for hideAll() and revealAll(), respectively
+// ****************************************************************************
+
+function hide1() {
+    hideFormerEmperors();
+    hideIchijoNoKuniNoNoramune();
+    hideNijoNoKiyoNoYataro();
+    hideSanjoNoKoujiNoMarusame();
+    hideMinamotoNoKumoNoAyakashi();
+    hideTairaNoSakamaNoYoshitomo();
+    hideFujiwaraNoSaiNoMineyoshi();
+    hideTachibanaNoYabuneNoKishihito();
+    hideMononobeNoYariNoSetsuna();
+    hideKujoNoShinaNoMotomori();
+    hideSogaNoYukaNoYaruhito();
+    hideAshikagaNoKuroneNoMariji();
+    hideTakatsukasaNoYamiNoMitsunoro();
+    hideTokugawaNoIyoNoSagashishige();
+    hideOinomikadoNoSuzuNoUma();
+    hideSaionjiNoNauNoKiyomune();
+    hideDaigoNoSaruNoOmune();
+    hideImadegawaNoGuntoNoSetsumaru();
+    hideTokudaijiNoHokoruNoMunesa();
+    hideKonoeNoMariNoSomezame();
+    hideKikuteiNoUmakaiNoHarumori();
+    hideKasannoinNoSumizomeNoHananokiyo();
+    hideYuuzakaNoAkaneNoShinuro();
+    return 0;
+}
+
+function reveal1() {
+    revealFormerEmperors();
+    revealIchijoNoKuniNoNoramune();
+    revealNijoNoKiyoNoYataro();
+    revealSanjoNoKoujiNoMarusame();
+    revealMinamotoNoKumoNoAyakashi();
+    revealTairaNoSakamaNoYoshitomo();
+    revealFujiwaraNoSaiNoMineyoshi();
+    revealTachibanaNoYabuneNoKishihito();
+    revealMononobeNoYariNoSetsuna();
+    revealKujoNoShinaNoMotomori();
+    revealSogaNoYukaNoYaruhito();
+    revealAshikagaNoKuroneNoMariji();
+    revealTakatsukasaNoYamiNoMitsunoro();
+    revealTokugawaNoIyoNoSagashishige();
+    revealOinomikadoNoSuzuNoUma();
+    revealSaionjiNoNauNoKiyomune();
+    revealDaigoNoSaruNoOmune();
+    revealImadegawaNoGuntoNoSetsumaru();
+    revealTokudaijiNoHokoruNoMunesa();
+    revealKonoeNoMariNoSomezame();
+    revealKikuteiNoUmakaiNoHarumori();
+    revealKasannoinNoSumizomeNoHananokiyo();
+    revealYuuzakaNoAkaneNoShinuro();
+    return 0;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /* ┏━━━━━━━━━━━━━━━━┓
  * ┃ 『全ての為に』のファンクション ┃
@@ -29,7 +501,15 @@
  */
 
 // Function for the "Hide all" / 『全てを隠す』 button
-function hideAll() {}
+function hideAll() {
+    hide0();
+    hide1();
+    return 0;
+}
 
 // Function for the "Reveal all" / 『全てを現す』 button
-function revealAll() {}
+function revealAll() {
+    reveal0();
+    reveal1();
+    return 0;
+}
