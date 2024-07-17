2aefeaf8e11e6434f8e3c28977f35d7006aec395
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-07-17 02:32:35 -0400

    Created accordion.js

---
+++ js/accordion.js
@@ -0,0 +1,22 @@
+/* ============================================================================
+ * accordion.js
+ * ============================================================================
+ * 
+ * 制作者：虹川郷鬼
+ * 
+ */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
