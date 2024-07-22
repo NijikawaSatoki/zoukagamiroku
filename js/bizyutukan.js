7531fe72030eb22d886cafdd627d0a52f7afc178
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-07-22 00:26:47 -0400

    Made function toggleNSFW()

--- js/bizyutukan.js
+++ js/bizyutukan.js
@@ -16,102 +16,19 @@
  * 
  */
 
-// *==========================================================================*
-// 『ＮＳＦＷコントロール』のファンクション
-// *==========================================================================*
-
-// Function for hiding NSFW art
-function hideNotSafeForWork() {
-    document.getElementById("nsfw0").style.display = "none";
-    document.getElementById("nsfw1").style.display = "none";
-    document.getElementById("nsfw2").style.display = "none";
-    document.getElementById("nsfw3").style.display = "none";
-    document.getElementById("nsfw4").style.display = "none";
-    document.getElementById("nsfw5").style.display = "none";
-    document.getElementById("nsfw6").style.display = "none";
-    document.getElementById("nsfw7").style.display = "none";
-    document.getElementById("nsfw8").style.display = "none";
-    document.getElementById("nsfw9").style.display = "none";
-    document.getElementById("nsfw10").style.display = "none";
-    document.getElementById("nsfw11").style.display = "none";
-    document.getElementById("nsfw12").style.display = "none";
-    document.getElementById("nsfw13").style.display = "none";
-    document.getElementById("nsfw14").style.display = "none";
-    document.getElementById("nsfw15").style.display = "none";
-    document.getElementById("nsfw16").style.display = "none";
-    document.getElementById("nsfw17").style.display = "none";
-    document.getElementById("nsfw18").style.display = "none";
-    document.getElementById("nsfw19").style.display = "none";
-    document.getElementById("nsfw20").style.display = "none";
-    document.getElementById("nsfw21").style.display = "none";
-    document.getElementById("nsfw22").style.display = "none";
-    document.getElementById("nsfw23").style.display = "none";
-    document.getElementById("nsfw24").style.display = "none";
-    document.getElementById("nsfw25").style.display = "none";
-    document.getElementById("nsfw26").style.display = "none";
-    document.getElementById("nsfw27").style.display = "none";
-    document.getElementById("nsfw28").style.display = "none";
-    document.getElementById("nsfw29").style.display = "none";
-    document.getElementById("nsfw30").style.display = "none";
-    document.getElementById("nsfw31").style.display = "none";
+// ＮＳＦＷをトグルするファンクション
+function toggleNSFW() {
+    let divNSFW = getElementsByClassName("nsfw");
+    if (window.getComputedStyle(divNSFW).display === "none") {
+        // ＯＮにする
+        divNSFW.style.display = "block";
+        return 0;
+    } else {
+        // ＯＦＦにする
+        divNSFW.style.display = "none";
+        return 0;
+    }
     return 0;
 }
 
-// Function for showing NSFW art
-function showNotSafeForWork() {
-    document.getElementById("nsfw0").style.display = "block";
-    document.getElementById("nsfw1").style.display = "block";
-    document.getElementById("nsfw2").style.display = "block";
-    document.getElementById("nsfw3").style.display = "block";
-    document.getElementById("nsfw4").style.display = "block";
-    document.getElementById("nsfw5").style.display = "block";
-    document.getElementById("nsfw6").style.display = "block";
-    document.getElementById("nsfw7").style.display = "block";
-    document.getElementById("nsfw8").style.display = "block";
-    document.getElementById("nsfw9").style.display = "block";
-    document.getElementById("nsfw10").style.display = "block";
-    document.getElementById("nsfw11").style.display = "block";
-    document.getElementById("nsfw12").style.display = "block";
-    document.getElementById("nsfw13").style.display = "block";
-    document.getElementById("nsfw14").style.display = "block";
-    document.getElementById("nsfw15").style.display = "block";
-    document.getElementById("nsfw16").style.display = "block";
-    document.getElementById("nsfw17").style.display = "block";
-    document.getElementById("nsfw18").style.display = "block";
-    document.getElementById("nsfw19").style.display = "block";
-    document.getElementById("nsfw20").style.display = "block";
-    document.getElementById("nsfw21").style.display = "block";
-    document.getElementById("nsfw22").style.display = "block";
-    document.getElementById("nsfw23").style.display = "block";
-    document.getElementById("nsfw24").style.display = "block";
-    document.getElementById("nsfw25").style.display = "block";
-    document.getElementById("nsfw26").style.display = "block";
-    document.getElementById("nsfw27").style.display = "block";
-    document.getElementById("nsfw28").style.display = "block";
-    document.getElementById("nsfw29").style.display = "block";
-    document.getElementById("nsfw30").style.display = "block";
-    document.getElementById("nsfw31").style.display = "block";
-    return 0;
-}
-
-// *--------------------------------------------------------------------------*
-
-// *==========================================================================*
-// 『全ての為に』のファンクション
-// *==========================================================================*
-
-// Function for the "Hide all" / 『全てを隠す』 button
-function hideAll() {
-    return 0;
-}
-
-// Function for the "Reveal all" / 『全てを現す』 button
-function revealAll() {
-    return 0;
-}
-
-// *--------------------------------------------------------------------------*
-
-// *==========================================================================*
-// アーカイブしたファンクション
-// *==========================================================================*
+///// ファイルの終わりです \\\\\
