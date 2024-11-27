4d6dfdc4bac2a1eaa9645f09955d4ba17cbee3e6
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-11-26 23:23:10 -0500

    The slate has been wiped clean

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/* The way I'm gonna do namespaces for functions and global variables is like 
+ * this: namespace_Name
+ * Hopefully that makes sense >_<'
+ * 
+ * <3 Satoki Nijikawa (Niji System)
+ */
+
+// =============================================================================
+// Hello World Example :3
+// =============================================================================
+
+// Prints `Hello World!' in an alert box
+function index_HelloWorld() {
+    let hello = "Hello World!";
+    window.alert(hello);
+    return 0;
+}
