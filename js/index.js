caa62f02fcab8053a7b144926f08d59e79a01d99
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-12-06 16:42:37 -0500

    Updated index.js Made the `Hello World' example less bollocks-y.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,7 @@
-/* The way I'm gonna do namespaces for functions and global variables is like 
- * this: namespace_Name
- * Hopefully that makes sense >_<'
- * 
- * <3 Satoki Nijikawa (Niji System)
- */
-
-// =============================================================================
-// Hello World Example :3
-// =============================================================================
+/* ====== Hello World Example :3 ====== */
 
 // Prints `Hello World!' in an alert box
-function index_HelloWorld() {
+function HelloWorld() {
     let hello = "Hello World!";
     window.alert(hello);
     return 0;
