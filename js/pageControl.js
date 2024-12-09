2e33349ff41962458ddf5f0522aa07a7e8034e72
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-12-09 04:22:06 -0500

    Updated pageControl.js Added some clarifying comments

--- js/pageControl.js
+++ js/pageControl.js
@@ -1,7 +1,15 @@
+// Toggle the infobox on and off
 function ToggleInfobox() {
-    
+    const INFOBOX = document.getElementById("Infobox");
+    if (INFOBOX.style.display === "none") {
+        INFOBOX.style.display = "block";
+    } else {
+        INFOBOX.style.display = "none";
+    }
+    return;
 }
 
+// Toggle between dark mode and light mode
 function ToggleTheme() {
     
 }
