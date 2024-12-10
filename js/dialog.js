a6888e4c19d2c6632f583b3e43fb07c75c795674
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-12-09 19:31:48 -0500

    Updated dialog.js Made an open function and a close function for the modals. Requires the ID of the button and the dialog's ID passed on as the function parametres.

--- js/dialog.js
+++ js/dialog.js
@@ -1,14 +1,9 @@
-// Constants
-const DIALOG = document.querySelector("dialog");
-const SHOW_BUTTON = document.querySelector("dialog + button");
-const CLOSE_BUTTON = document.querySelector("dialog button");
-
-// "Show the dialog" button opens the dialog modally
-SHOW_BUTTON.addEventListener("click", () => {
-    DIALOG.showModal();
-});
-
-// "Close" button closes the dialog
-CLOSE_BUTTON.addEventListener("click", () => {
-    DIALOG.close();
-});
+// Open and close the dialog modal :3
+function OpenModal(btnId, dialogId) {
+  let dialog = document.getElementById(dialogId);
+  dialog.showModal();
+}
+function CloseModal(btnId, dialogId) {
+  let dialog = document.getElementById(dialogId);
+  dialog.close();
+}
